786cb178c4ace9ef6a5031e4ea6efbccf6f9f4c6
ph04 <tacc010203@gmail.com>
2023-04-29 19:45:51 +0200

    what on earth

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define ENDED_TEXT 0
+#define NOT_ENDED_TEXT 1
+#define ENDED_PARAGRAPH 2
+
 typedef struct {
     char* content;
 
@@ -216,19 +220,38 @@ bool is_char(char character) {
     return character != ' ' && character != '\n' && character != '\0';
 }
 
-bool read_chunk(char* input_content, char* line_chunk_content, int w_col, int* base_idx) {
+void pad_string(char* string, int start, int len, char character) {
+    for (int i = start; i < len; i++) {
+        string[i] = character;
+    }
+}
+
+int read_chunk(char* input_content, char* line_chunk_content, int w_col, int* base_idx) {
     bool is_text_started = false;
 
-    int i = *base_idx;
-    int finish = i + w_col;
+    // int i = *base_idx;
+    int finish = *base_idx + w_col;
 
-    for (int j = i; j < finish; j++) {
+    for (int j = *base_idx; j < finish; j++) {
         char curr_char = input_content[j];
         char next_char = input_content[j + 1];
+
+        if (curr_char == '\n' && next_char == '\n') {
+            pad_string(line_chunk_content, j - finish + w_col, w_col, ' ');
+
+            char DEBUG = input_content[*base_idx];
+            *base_idx -= w_col - (j - finish + w_col) + 2;
+            DEBUG = input_content[*base_idx];
+            // *base_idx += 2;
+
+            return ENDED_PARAGRAPH;
+        }
         
+        // TODO: provare a vedere se il tutto funziona lasciando spazi multipli dentro una riga, sempre che io lo voglia fare
         if (curr_char != '\0') {
             if ((!is_text_started && !is_char(curr_char)) || (is_text_started && !is_char(curr_char) && !is_char(next_char))) { // TODO: controlla \n\n
                 finish++;
+                // *base_idx++;
                 *base_idx += 1;
             } else {
                 // printf("%c", input_content[j]);
@@ -237,11 +260,11 @@ bool read_chunk(char* input_content, char* line_chunk_content, int w_col, int* b
                 line_chunk_content[j - finish + w_col] = curr_char; // j - i - (finish - i - w_col)
             }
         } else {
-            return true;
+            return ENDED_TEXT;
         }
     }
 
-    return false;
+    return NOT_ENDED_TEXT;
 }
 
 bool check_truncated_end(char* line_chunk_content, int w_col, char next_char) {
@@ -304,12 +327,6 @@ int min(int x, int y) {
     return (x < y) ? x : y;
 }
 
-void initialize(char queue[], int len) {
-    for (int i = 0; i < len; i++) {
-        queue[i] = '\0';
-    }
-}
-
 void enqueue(char queue[], int len, int* head, int* tail, char element) {
     if (*tail >= 0 && *tail < len - 1) {
         queue[*tail] = element;
@@ -336,7 +353,7 @@ void slide_characters(char* string, int len, int spaces_end, int ratio) {
 
     char queue[len];
 
-    initialize(queue, len);
+    pad_string(queue, 0, len, '\0');
 
     for (int i = 1; i < len; i++) {
         char DEBUG = string[i];
@@ -438,7 +455,7 @@ Page* build_pages(char* input_content, int cols, int h_col, int w_col) {
     for (int i = 0; input_content[i] != '\0'; i += w_col) {
         char* line_chunk_content = calloc(w_col + 1, sizeof(char));
 
-        bool end = read_chunk(input_content, line_chunk_content, w_col, &i);
+        int end_value = read_chunk(input_content, line_chunk_content, w_col, &i);
 
         if (check_truncated_end(line_chunk_content, w_col, input_content[i + w_col])) {
             if (no_spaces(line_chunk_content, w_col)) {
@@ -451,9 +468,12 @@ Page* build_pages(char* input_content, int cols, int h_col, int w_col) {
         string_replace(line_chunk_content, '\n', ' ');
 
         // printf("%s %d %d\n", line_chunk_content, strlen(line_chunk_content), w_col);
+
         printf("%s\n", line_chunk_content);
 
-        justify_string(line_chunk_content, w_col);
+        if (end_value != ENDED_PARAGRAPH) {
+            justify_string(line_chunk_content, w_col);
+        }
 
         printf("%s\n", line_chunk_content);
 
@@ -469,7 +489,7 @@ Page* build_pages(char* input_content, int cols, int h_col, int w_col) {
 
         set_line_chunks_head(curr_line);
 
-        if (end) {
+        if (end_value == ENDED_TEXT) {
             break;
         }
 
@@ -503,9 +523,9 @@ Page* build_pages(char* input_content, int cols, int h_col, int w_col) {
 int main(int argc, char* argv[]) {
     // TODO: file reading
     // char* input_content = "Sistemi Operativi II Modulo. Progetto (Homework).\n\nL’obiettivo è implementare un programma C che trasformi un testo in italiano da una colonna a più colonne su più pagine (come ad es. per una pagina di quotidiano). Sono richieste due versioni del programma: una a singolo processo e una multi-processo con almeno tre processi concorrenti.\n\nDati in ingresso e requisiti: un file di testo in codifica Unicode (UTF-8) contenente un testo in italiano strutturato in paragrafi separati da una o più linee vuote (stringa contenente solo il carattere ‘\n’). Ogni paragrafo è  costituito da una o più stringhe terminate da ‘\n’ (il testo in ingresso è tutto su una sola colonna); il numero di colonne per pagina su cui distribuire il testo; il numero di linee per ogni colonna; larghezza di ogni colonna (numero di caratteri per colonna); la distanza in caratteri tra una colonna di testo e l’altra.\n\nDati in uscita e requisiti: un file di testo ASCII contenente il testo di input incolonnato e impaginato secondo i parametri espressi dall’utente. Le pagine sono separate dalla stringa “\n %%% \n”; ogni paragrafo è separato da una linea vuota; la prima colonna di testo è a sinistra; tutte le righe, tranne l’ultima, di ogni paragrafo dovranno essere allineate ad entrambi i margini della colonna; l’ultima riga di ogni paragrafo è solo allineata a sinistra; le parole in una linea dovranno essere separate da almeno un carattere di spazio; la sillabazione di eventuali parole troppo lunghe non è necessaria.\n\nRequisiti generali del progetto: ogni file .c/.h  dovrà essere ben commentato: per ogni funzione commentare brevemente i parametri di ingresso/uscita e il suo funzionamento generale; nel corpo di ogni funzione commentare le linee di codice più importanti; fornire un makefile per compilare il progetto con un semplice make; fornire un file di testo README con una breve spiegazione dei file inclusi e un breve manuale utente; la versione multi-processo del programma dovrà essere costituita da almeno tre processi concorrenti ed intercomunicanti: uno per leggere il file di ingresso, uno per creare le colonne, ed infine uno per la scrittura del file di output. (Ulteriori suddivisioni del carico di lavoro in più di tre processi sono ammesse.) NON è ammesso l’uso di librerie esterne con funzioni per la manipolazione di stringhe o testo che non siano quelle standard del C. Eccezione: è ammesso l’uso di librerie esterne per l’analisi delle opzioni della linea di comando per il vostro programma.\n\nSuggerimenti: usare nomi di variabili e funzioni corrispondenti al loro significato/utilizzo; usare stdin e stdout per rispettivamente il testo in ingresso e in uscita; sviluppare prima la versiosne mono-processo del programma, poi quella multi-processo; per l’analisi della linea di comando si consiglia getopt, di uso molto semplice e che fa chunke della libreria GNU standard del C; concentratevi prima sulla correttezza dei programmi, cioè assicuratevi con più testi di input che i programmi producano un output incolonnato secondo le richieste. Se avete tempo alla fine, ottimizzate il codice per uso di CPU e/o RAM, mantenendone la correttezza; potete usare wc per verificare che il numero delle parole in ingresso e in uscita dei vostri programmi sia lo stesso (usando stdin e stdout nel vostri programmi, questo semplice test si riduce ad una sola linea di comando con l’operatore pipe | della shell).\n\nIl punteggio massimo è 6/30, articolato come segue: correttezza del programma (3/30); architettura del programma e commenti (2/30); usabilità e istruzioni utente (1/30).";
-    // char* input_content = "lorem ipsum dolor sit amet"; // TODO: FIXA PROBLEMA DELLE PAROLE PRECISE
+    // char* input_content = "lorem ipsum dolor sit amet";
     // char* input_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod mauris ac malesuada sagittis. Morbi imperdiet ullamcorper tortor, eget varius sem ultrices a. Nam molestie dignissim lectus, et dapibus nunc tristique in. Sed eget neque egestas, aliquam lacus non, rhoncus ex. Suspendisse metus orci, gravida eget gravida ullamcorper, accumsan dapibus justo. Donec elit libero, ullamcorper id interdum et, porttitor non metus. In ultrices non arcu tincidunt scelerisque. Morbi at lectus quis nunc faucibus mattis id et ex. Ut viverra nunc arcu, at mollis orci ultricies non. Etiam tempor euismod neque eget imperdiet. Fusce faucibus sem velit, at pellentesque odio sagittis vel. Duis hendrerit dui sed maximus suscipit. Praesent tristique quam erat, eu mollis ante malesuada a. Phasellus rutrum tempus lorem, vel vulputate ante lobortis in. Nam lacinia ut arcu vitae suscipit.\n\nMauris volutpat purus placerat lacus vestibulum, sit amet bibendum arcu tincidunt. Fusce volutpat orci in ex suscipit euismod. Donec mi nibh, blandit vitae ligula non, cursus fermentum dolor. Mauris sit amet venenatis nisi. Curabitur quis dolor at metus consectetur congue. Ut velit nunc, ultrices sed convallis eu, posuere a arcu. Vivamus tristique finibus sapien, vitae lacinia ante dapibus eget. Praesent libero nisl, efficitur vel ex in, semper tempor erat. Aliquam pharetra mi nec nisi varius, nec sollicitudin ligula pulvinar. Curabitur bibendum arcu quis erat tincidunt, sed tempus ex placerat. Donec ornare, tellus pulvinar pellentesque elementum, arcu ante pharetra metus, ut malesuada metus nibh ut orci. Aenean pulvinar nisi arcu, a pharetra mi ornare nec.";
-    char* input_content = "ciao a tutti sono il capobastone come va spero tutto bene, io sto molto bene      mi      sento veramente fortissimo ciao a tutti quanti.\n\nquesta e' una prova di paragrafo, speriamo non esploda tutto quanto.\n\nciao a tutti sono il capobastone come va spero tutto bene, io sto molto bene mi sento veramente fortissimo ciao a tutti quanti.\n\nquesto e' una prova di paragrafo, speriamo non esploda tutto qua    nto.";
+    char* input_content = "ciao a tutti sono il capobastone come va spero tutto bene, io sto molto bene      mi      sento veramente fortissimo ciao a tutti.\n\nquesta e' una prova di paragrafo, speriamo non esploda tutto quanto.\n\nciao a tutti sono il capobastone come va spero tutto bene, io sto molto bene mi sento veramente fortissimo ciao a tutti quanti.\n\nquesto e' una prova di paragrafo, speriamo non esploda tutto qua    nto.";
     // char* input_content = "questo e' un super test che mi serve per capire se quello che sto facendo sta effettivamente funzionando oppure non sta funzionando proprio assolutamente niente, ti prego funzona pls.\n\nah e questa e' una prova di paragrafo.";
     // char* input_content = "a                        b";
 
