5dd4115d7bc6d6241b30dd30ff5894de373f24c7
ph04 <tacc010203@gmail.com>
2023-05-02 16:26:05 +0200

    testalo che non so se funziona

--- src/main.c
+++ src/main.c
@@ -1,66 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 
-#include "string_tools.h"
+#include "args.h"
 #include "page.h"
 
-#define ARGS_SUCCESS 0
-#define NOT_ENOUGH_ARGS -1
-#define CHARS_IN_NUMERIC_ARG -2
-#define PRINT_HELP -3
-
-// TODO: mettere nella doc che se il file è NULL restituisce true
-bool is_file_empty(FILE* file) {
-    if (file != NULL) {
-        fseek(file, 0, SEEK_END);
-
-        if (ftell(file) != 0) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-int parse_args(int argc, char* argv[], char** input_path, char** output_path, int* cols, int* h_col, int* w_col, int* spacing) {
-    if (argc < 7) {
-        if (argv[1] == "--help") {
-            return PRINT_HELP;
-        }
-
-        return NOT_ENOUGH_ARGS;
-    }
-
-    *input_path = argv[1];
-    *output_path = argv[2];
-
-    char* raw_cols = argv[3];
-    char* raw_h_col = argv[4];
-    char* raw_w_col = argv[5];
-    char* raw_spacing = argv[6];
-
-    if (ATOI_INVALID_CHAR == pos_atoi(raw_cols, strlen(raw_cols), cols)) {
-        return CHARS_IN_NUMERIC_ARG;
-    }
-
-    if (ATOI_INVALID_CHAR == pos_atoi(raw_h_col, strlen(raw_h_col), h_col)) {
-        return CHARS_IN_NUMERIC_ARG;
-    }
-
-    if (ATOI_INVALID_CHAR == pos_atoi(raw_w_col, strlen(raw_w_col), w_col)) {
-        return CHARS_IN_NUMERIC_ARG;
-    }
-
-    if (ATOI_INVALID_CHAR == pos_atoi(raw_spacing, strlen(raw_spacing), spacing)) {
-        return CHARS_IN_NUMERIC_ARG;
-    }
-}
+#define PROGRAM_SUCCESS 0
+#define INVALID_INPUT_FILE -1
+#define INVALID_OUTPUT_FILE -2
+#define NON_EMPTY_OUTPUT_FILE -3
+#define ARGS_FAILURE -4
+#define ALLOCATION_FAILURE -5
 
 int main(int argc, char* argv[]) {
-    // TODO: grab this from argv
-    // check if everything is != 0
-
     // int cols = 3;
     // int h_col = 40;
     // int w_col = 21;
@@ -82,25 +32,24 @@ int main(int argc, char* argv[]) {
     int args_err = parse_args(argc, argv, &input_path, &output_path, &cols, &h_col, &w_col, &spacing);
 
     switch (args_err) {
-        case ARGS_SUCCESS:
-            // TODO: IL PROGRAMMA VA RUNNATO QUA.
-            break;
         case NOT_ENOUGH_ARGS:
             fprintf(stderr, "Not enough argument passed, 7 arguments expected.\n\nSee '--help' for more information.\n");
             break;
         case CHARS_IN_NUMERIC_ARG:
             fprintf(stderr, "The last 4 arguments are expected to be strictly positive integers, but non-digits characters where found.\n\nSee '--help' for more information.\n");
             break;
+        case ZERO_INTEGER:
+            fprintf(stderr, "0 as value is not allowed.\n\nSee '--help' for more information.\n");
+            break;
         case PRINT_HELP:
             fprintf(stderr, "TODO: FAI L'HELP\n");
             break;
         default:
-            // unreachable code
             break;
     }
     
     if (args_err != ARGS_SUCCESS) {
-        return -1;
+        return ARGS_FAILURE;
     }
 
     // TODO: fai diventare questi tipo dei test, CHE SERVE ANCHE PER QUANDO LO DEVI RIFARE MULTIPROCESSORE
@@ -111,34 +60,34 @@ int main(int argc, char* argv[]) {
     // char* input_content = "a                        b";
     // char* input_content = "a x x b x x c c iaone come stae"; h_col = 7; w_col = 18; // TODO: controlla quando arriva all'ultimo (devo pensare a com'è fatto il test)
 
-    // FILE* input_file = fopen("lorem_long.txt", "r"); // COMPILATION
-    // FILE* input_file = fopen("../lorem_long.txt", "r"); // DEBUG
-
-    // FILE* input_file = fopen("utf8_text.txt", "r"); // COMPILATION
-    // FILE* input_file = fopen("../utf8_text.txt", "r"); // DEBUG
-
     FILE* input_file = fopen(input_path, "r");
     FILE* output_file = fopen(output_path, "a");
 
     if (input_file == NULL) {
-        fprintf(stderr, "INPUT FILE: TODO ERROR HANDLING\n");
+        fprintf(stderr, "An error occurred while trying to open the input file '%s'.\n", input_path);
         
-        return -1;
+        return INVALID_INPUT_FILE;
     }
 
     if (output_file == NULL) {
-        fprintf(stderr, "OUTPUT FILE: TODO ERROR HANDLING\n");
+        fprintf(stderr, "An error occurred while trying to open the output file '%s'.\n", output_path);
 
-        return -1;
+        return INVALID_OUTPUT_FILE;
     }
 
     if (!is_file_empty(output_file)) {
-        printf("Output file '%s' is not empty, the program will not run.\n\nSee '--help' for more information.", output_path);
+        fprintf(stderr, "Output file '%s' is not empty, the program will not run.\n\nSee '--help' for more information.", output_path);
 
-        return -1;
+        return NON_EMPTY_OUTPUT_FILE;
     }
 
     Page* pages = new_page(NULL);
+
+    if (pages == NULL) {
+        fprintf(stderr, "An error occurred while trying to allocate memory in the program.\n");
+
+        return ALLOCATION_FAILURE;
+    }
 
     int exit_code = build_pages(input_file, pages, cols, h_col, w_col);
 
@@ -146,20 +95,16 @@ int main(int argc, char* argv[]) {
         case REALLOC_ERROR:
             fprintf(stderr, "REALLOC: TODO ERROR HANDLING\n");
 
-            // break;
+            break;
         case INSUFFICIENT_WIDTH:
             fprintf(stderr, "WIDTH: TODO ERROR HANDLING\n");
 
-            // break;
+            break;
         case PAGE_SUCCESS:
-
             print_pages(output_file, pages, spacing, "\n%%%\n\n", ' '); // TODO: se questi sono NULL che succede? ehehe
-
-            printf("Successfully printed the output on '%s'.\n", output_path);
 
             break;
         default:
-            // unreachable code
             break;
     }
 
@@ -167,5 +112,5 @@ int main(int argc, char* argv[]) {
 
     fclose(input_file); // TODO: gestire l'errore (che me invento?!)
 
-    return 0;
+    return PROGRAM_SUCCESS;
 }
