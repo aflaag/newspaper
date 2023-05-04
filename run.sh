d6a167ca4270217308d8358b077277f652f087f3
ph04 <tacc010203@gmail.com>
2023-05-04 10:44:44 +0200

    non funziona niente off

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/sh
 
 # FAI LA FUNZIONE FATTA BENE CHE RAPPRESENTA UN SINGOLO TEST
+# E FAGLI STAMPARE IL NOME DEL FILE AL POSTO DEL NUMERO
 
 flush_output() {
     rm -f output.txt
@@ -33,14 +34,20 @@ python tester.py output.txt expected_outputs/capo_out.txt
 
 flush_output
 
-# SPACING 1 HA W = 18
-
 echo -n "Test 5: "
-./newspaper sample_inputs/spacing2_in.txt output.txt 3 7 21 1
-# python tester.py output.txt expected_outputs/lorem_short_out.txt
+./newspaper sample_inputs/spacing_in.txt output.txt 3 7 18 1
+python tester.py output.txt expected_outputs/spacing_out.txt
 
-# flush_output
+flush_output
 
-# echo -n "Test 6: "
-# ./newspaper sample_inputs/super_short_in.txt output.txt 3 7 18 1
-# python tester.py output.txt expected_outputs/super_short_out.txt
+echo -n "Test 6: "
+./newspaper sample_inputs/spacing2_in.txt output.txt 3 7 18 1
+python tester.py output.txt expected_outputs/spacing2_out.txt
+
+flush_output
+
+echo -n "Test 7: "
+./newspaper sample_inputs/super_short_in.txt output.txt 3 7 21 1
+python tester.py output.txt expected_outputs/super_short_out.txt
+
+flush_output
